4d8369f8032502ecd16d1bf3ca9295e120056494
Dimitris Vogiatzis <dvogiatzis@confluent.io>
2023-12-25 18:18:38 +0200

    Refactor Code

--- SideTrayView.ts
+++ SideTrayView.ts
@@ -1,37 +1,33 @@
-import { log } from "console";
 import {
 	ItemView,
 	MarkdownRenderer,
 	TFile,
 	WorkspaceLeaf,
-	moment,
+	moment
 } from "obsidian";
-import { getAllWeeklyNotes, getDailyNote, getDateUID } from "obsidian-daily-notes-interface";
+import { getDailyNote } from "obsidian-daily-notes-interface";
 import { get } from "svelte/store";
 import { dailyNotesStore } from "ui/stores";
 
-
 export const VIEW_TYPE = "super-scratchpad";
-let timer: NodeJS.Timeout | null = null;
 
 export class SideTrayView extends ItemView {
 
-	private today;
+	private timer: NodeJS.Timeout | null = null;
+	private today: moment.Moment;
 
 	private baseContainer: HTMLElement;
 
 	constructor(leaf: WorkspaceLeaf) {
 		super(leaf);
-		console.log("After super");
-		
 		this.onFileModified = this.onFileModified.bind(this);
+		this.openSelectedDayNoteInRootLeaf = this.openSelectedDayNoteInRootLeaf.bind(this);
 		this.registerEvent(this.app.vault.on("modify", this.onFileModified));
 		this.today = moment();
 		this.baseContainer = document.createElement('div');
 		this.baseContainer.id = "super-scratchpad"
 		this.containerEl.empty();
-		this.containerEl.appendChild(this.baseContainer)	
-		console.log("Called constructor");
+		this.containerEl.appendChild(this.baseContainer)
 	}
 
 	getViewType() {
@@ -41,8 +37,6 @@ export class SideTrayView extends ItemView {
 	getDisplayText() {
 		return "Super Scratchpad";
 	}
-
-	private timer: NodeJS.Timeout | null = null; // Define timer at class level
 
 	private async onFileModified(file: TFile): Promise<void> {
 
@@ -58,46 +52,72 @@ export class SideTrayView extends ItemView {
 			// Place operations that you want to perform after the delay here
 		}, 1000);
 
-		this.renderAllData();
+		this.mergeAllDailyNotesAndDisplay();
 	}
 
 	async onOpen(): Promise<void> {
-		this.renderAllData();
+		this.mergeAllDailyNotesAndDisplay();
 	}
 
-	async renderAllData() {
+	private async mergeAllDailyNotesAndDisplay() {
+		this.baseContainer.empty();
+
+		const mergedDailyNotes = await this.mergeAllDailyNotes();
+
+		MarkdownRenderer.render(this.app, mergedDailyNotes, this.baseContainer, "", this);
+
+		this.attachOnClickEventHandlerToDateLinks();
+	}
+
+	private async mergeAllDailyNotes() {
 		dailyNotesStore.reindex();
 		const allNotes: Record<string, TFile> = get(dailyNotesStore);
-		const todayNote: TFile = getDailyNote(this.today, allNotes);
-		const allMoments = Object.keys(allNotes).map(k => k.replace(/^day-/i, '')).map(v => moment(v));
+		const allMoments: moment.Moment[] = Object.keys(allNotes).map(k => k.replace(/^day-/i, '')).map(v => moment(v));
 
-		console.log(allNotes);
-
-		var trayData: string[] = [];
+		var headingsAndNotesForEachDay: string[] = [];
 		for (const thatMoment of allMoments) {
 			const thatDatesNote = getDailyNote(thatMoment, allNotes);
 			const thatDatesData = await this.app.vault.adapter.read(thatDatesNote.path);
-			trayData.push(`# ${thatMoment.format("DD.MM.YYYY")}[Go](${thatMoment.format("DD.MM.YYYY")})\n`)
-			trayData.push(thatDatesData + '\n');
+			headingsAndNotesForEachDay.push(`# [${thatMoment.format("DD.MM.YYYY")}](${thatMoment.format("DD.MM.YYYY")})\n`);
+			headingsAndNotesForEachDay.push(thatDatesData + '\n');
 		}
 
-		const pairs: string[] = [];
-		for (let i = 0; i < trayData.length; i += 2) {
-			const pair = trayData.slice(i, i + 2).join('\n');
-			pairs.push(pair);
+		const headingAndContentPairs: string[] = [];
+		for (var i = 0; i < headingsAndNotesForEachDay.length; i += 2) {
+			const headingAndContentPair = headingsAndNotesForEachDay.slice(i, i + 2).join('\n');
+			headingAndContentPairs.push(headingAndContentPair);
 		}
 
-		const result = pairs.join('\n---\n');
+		const mergedDailyNotes = headingAndContentPairs.join('\n---\n');
+		return mergedDailyNotes;
+	}
 
-		this.baseContainer.empty();
+	private attachOnClickEventHandlerToDateLinks() {
+		const dateLinks = document.querySelectorAll('#super-scratchpad h1>a.internal-link');
 
-		MarkdownRenderer.render(
-			this.app,
-			result,
-			this.baseContainer,
-			todayNote.path,
-			this
-		);
+		dateLinks.forEach((link: HTMLAnchorElement) => {
+			link.addEventListener('click', this.openSelectedDayNoteInRootLeaf);
+		});
+	}
+
+	private async openSelectedDayNoteInRootLeaf(event: MouseEvent) {
+		const tFile = this.getTFileForClickedNote(event);
+		if (tFile == null) return
+		this.openTFileInRootLeaf(tFile)
+	}
+
+	private getTFileForClickedNote(event: MouseEvent): TFile | null {
+		const element = event.target as HTMLAnchorElement;
+		const noteName: string | null = element.getAttribute('href');
+		const path = 'Journal/' + noteName + '.md';
+		return this.app.vault.getAbstractFileByPath(path) as TFile | null;
+	}
+
+	private async openTFileInRootLeaf(tFile: TFile) {
+		const newLeaf: WorkspaceLeaf = this.app.workspace.getLeaf(true);
+		await newLeaf.setViewState({ type: "tab", active: true });
+		newLeaf.openFile(tFile)
+		this.app.workspace.revealLeaf(newLeaf)
 	}
 
 	async onClose() {
--- main.ts
+++ main.ts
@@ -48,15 +48,7 @@ export default class MyPlugin extends Plugin {
 
 		await this.loadSettings();
 
-		console.log("Loading");
-
-		this.registerView(VIEW_TYPE, (leaf) => {
-			console.log("Registering");
-			
-			return new SideTrayView(leaf);
-		});
-
-		console.log("Loaded");
+		this.registerView(VIEW_TYPE, (leaf) => new SideTrayView(leaf));
 
 
 		this.addRibbonIcon("dice", "Activate view", () => {
@@ -159,16 +151,12 @@ export default class MyPlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
+		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// // Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+		// 	console.log("click", evt);
+		// });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() { }
